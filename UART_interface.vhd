9e5ea049b70e8f0adf54cbf5fa9fcd58ae3274b1
Joaquin <jortizmez@gmail.com>
2021-01-02 13:10:00 +0100

    The interface is done, now all pieces have to be put together. A testbench is still missing.

--- UART_interface.vhd
+++ UART_interface.vhd
@@ -19,15 +19,16 @@ entity hc_sr04Touart is
         Data_i  : in std_logic_vector(31 downto 0);
         Data_o  : out std_logic(31 downto 0);
 
-        WE_i        : in std_logic;
+        WEn_i        : in std_logic;
         Valid_i     : in std_logic;
+        Ack_o       : out std_logic;
         Tx_Ready_o  : out std_logic;
         Rx_Ready_o  : out std_logic;
 
         Cfg_done_o  : out std_logic;
 
         TX_o        : out std_logic;  --UART TX
-        RX_o        : in std_logic   --UART RX
+        RX_i        : in std_logic   --UART RX
     );
 end entity hc_sr04Touart;
 
@@ -48,7 +49,6 @@ architecture arch of ent is
     signal Data_i_s     : std_logic_vector(31 downto 0);
     signal Config_done_s    : std_logic := '0';
 
-
 begin
 
     Data_i_s <= Data_i when Config_done_s = '1' else DCfg_i_s;
@@ -68,67 +68,52 @@ begin
 		TX_Interrupt       => Tx_Ready_o,
 		RX_Interrupt       => Rx_Ready_o,		
 		-- Port Pins
-		RxD                => RX_o,
+		RxD                => RX_i,
 		TxD                => TX_o
     );
 
     Config:block
 
     --Typ for state values
-    type state_type is (IDLE, CONFIG, DONE, S_ERROR);
-    type sub_states is (LOAD_CTRL, WAIT_ACK)
+    type state_type is (CONFIG, DONE, WAIT_ACK, S_ERROR);
 
     --Intern signals from the control unit
-
-    signal State      : state_type := IDLE;
-    signal Next_State : state_type;
-    signal SubState   : sub_states := LOAD_CTRL;
-    signal Next_SubState : sub_states;
+    signal State            : state_type := CONFIG;
+    signal Next_State       : state_type;
 
     begin
 
-        process( Config_done_s )
-        begin
-            Cfg_done_o <= Config_done_s;
-        end process ;    
-        
-        Transition: process(State, SubState, Valid_i, ACK_s)
+        --Signal to the output
+        Cfg_done_o <= Config_done_s;
+
+        Transition: process(State, ACK_s)
         begin
 
             case( State ) is
-            
-                when IDLE =>
-                        if Valid_i = '1' then
-                            Next_State <= CONFIG;
-                        else
-                            Next_State <= IDLE;
-                        end if;
+
                 when CONFIG =>
-                            case( SubState ) is
-                            
-                                when LOAD_CTRL =>
-                                            DCfg_i_s(15 downto 0) <= std_logic_vector(to_unsigned(BitWidthM1_g, 16));
-                                            DCfg_i_s(19 downto 16) <= std_logic_vector(to_unsigned(BitsM1_g, 4));
-                                            DCfg_i_s(20) <= '1' when Parity_on_c = 1 else '0';
-                                            DCfg_i_s(21) <= '1' when Parity_odd_c = 1 else '0';
-                                            DCfg_i_s(23 downto 22) <= std_logic_vector(to_unsigned(StopBits_c, 2));
-                                            DCfg_i_s(24 downto 25) <= "11";
+                            DCfg_i_s(15 downto 0) <= std_logic_vector(to_unsigned(BitWidthM1_g, 16));
+                            DCfg_i_s(19 downto 16) <= std_logic_vector(to_unsigned(BitsM1_g, 4));
+                            DCfg_i_s(20) <= '1' when Parity_on_c = 1 else '0';
+                            DCfg_i_s(21) <= '1' when Parity_odd_c = 1 else '0';
+                            DCfg_i_s(23 downto 22) <= std_logic_vector(to_unsigned(StopBits_c, 2));
+                            DCfg_i_s(24 downto 25) <= "11";
 
-                                            --Set the control bus signals
-                                            WE_s <= '1';        --Write
-                                            ADR_s <= x"8";      --To the control register
-                                            STB_s <= '1';       --strobe signal
+                            --Set the control bus signals
+                            WE_s <= '1';        --Write
+                            ADR_s <= x"8";      --To the control register
+                            STB_s <= '1';       --strobe signal
 
-                                            Next_SubState <= WAIT_ACK,                      
-                                when WAIT_ACK =>
-                                            if ACK_s <= '1' then
-                                                STB_s <= '0';       --strobe signal
-                                                Next_State <= DONE;
-                                            else
-                                                Next_SubState <= WAIT_ACK,
-                                            end if ;
-                                when others => null;
-                            end case ;
+                            Next_State <= WAIT_ACK;
+
+                when WAIT_ACK =>
+                            if ACK_s <= '1' then
+                                STB_s <= '0';       --strobe signal
+                                Next_State <= DONE;
+                            else
+                                Next_State <= WAIT_ACK,
+                            end if ;
+
                 when DONE => Next_State <= DONE;
                 when S_ERROR => null,
             end case ;
@@ -139,18 +124,15 @@ begin
         begin
             if rising_edge (clk_i) then
                 if rst_i = '1' then
-                    State <= IDLE;
-                    SubState <= LOAD_CTRL;
+                    State <= CONFIG;
                 else
                     State <= Next_State;
-                    SubState <= Next_SubState;
                 end if;
 
                 if  rst_i = '1' then
                     Config_done_s <= '0';
                 else
                     case( Next_State ) is
-                        when IDLE    => Config_done_s <= '0';
                         when CONFIG  => Config_done_s <= '0';
                         when DONE    => Config_done_s <= '1';
                         when S_ERROR => null;
@@ -158,6 +140,33 @@ begin
                 end if ;
             end if ;
         end process;
+
+    end block;
+
+    Operation:block
+    begin
+
+        DataTransmision: process( clk_i )
+        begin
+
+            if rising_edge(clk_i) then
+
+                WE_s <= WEn_i;
+                STB_s <= Valid_i;
+
+                if Valid_i = '1' and WEn_i = '1' then
+                    --Write data to be trasmited
+                    ADR_s <= x"0";
+                elsif Valid_i = '1' and WEn_i = '0' then
+                    --Read data received from UART
+                    ADR_s <= x"4";
+                end if ;
+            end if ;
+            
+        end process ; -- Transition
+        
+        --ACK signal from UART to the output 
+        Ack_o <= ACK_s;
 
     end block;
 
